54e89d9f6dd81556a2bdbc4f21562aa42b4a5724
Ilya <mr.ilya.zhukov.2004@mail.ru>
2023-10-03 16:58:04 +0300

    изменения

--- part1.cs
+++ part1.cs
@@ -1,81 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _3laba
+public class PriorityQueue<T>
 {
-    public class PriorityQueue<T>
+    //объявляет приватное поле queue типа List<T> для хранения элементов очереди
+    private List<T> queue;
+    //oбъявляет приватное поле priorities типа List<int> для хранения приоритетов элементов очереди.
+    private List<int> priorities;
+
+    //Создает новые экземпляры List<T> и List<int> для queue и priorities соответственно.
+    public PriorityQueue()
     {
-        //объявление приватного поля `queue`, которое будет использоваться для
-        //хранения элементов и их приоритетов в виде списка кортежей.
-        private List<Tuple<T, int>> queue;
-        //конструктор класса, который инициализирует список `queue`.
-        public PriorityQueue()
-        {
-            queue = new List<Tuple<T, int>>();
-        }
-
-        //метод, который добавляет элемент и его приоритет в очередь
-        public void Enqueue(T item, int priority)
-        {
-            //Кортеж добавляется в список `queue`.
-            queue.Add(new Tuple<T, int>(item, priority));
-            //Список сортируется в порядке убывания приоритетов элементов.
-            queue.Sort((x,y) => y.Item2.CompareTo(x.Item2));
-        }
-
-        //метод извлекает элемент с наивысшим приоритетом из очереди
-        public T Dequeue()
-        {
-            //Проверяется, что список `queue` не пустой, иначе вызывается исключение.
-            if (queue.Count == 0)
-                throw new InvalidOperationException("Queue is empty");
-            //Извлекается элемент с наивысшим приоритетом из списка `queue`.
-            T item = queue[0].Item1;
-            //Элемент удаляется из списка `queue`.
-            queue.RemoveAt(0);
-            //Извлеченный элемент возвращается.
-            return item;
-        }
-        //свойство `Count`, которое возвращает количество элементов в очереди.
-        public int Count
-        {
-            get { return queue.Count; }
-        }
-        //свойство `IsEmpty`, которое возвращает `true`, если очередь пуста, иначе `false`.
-        public bool IsEmpty
-        {
-            get { return queue.Count == 0; }
-        }
-
-        public void Clear()
-        {
-            queue.Clear();
-        }
+        queue = new List<T>();
+        priorities = new List<int>();
     }
-    public class Program
+
+    //метод для добавления элемента item с приоритетом priority в очередь.
+    //Он находит правильную позицию в очереди, основываясь на приоритете, и вставляет элемент и приоритет в соответствующие списки.
+    public void Enqueue(T item, int priority)
     {
-        public static void Main(string[] args)
+        int index = queue.Count;
+        while (index > 0 && priority <= priorities[index - 1])
         {
-            //создание экземпляра класса
-            PriorityQueue<string> queue = new PriorityQueue<string>();
+            index--;
+        }
 
-            //вызов метода `Enqueue` для добавления элемента "Item 1" с приоритетом 3 в очередь.
-            queue.Enqueue("Item 1", 3);
-            //вызов метода `Enqueue` для добавления элемента "Item 2" с приоритетом 1 в очередь.
-            queue.Enqueue("Item 2", 1);
-            //вызов метода `Enqueue` для добавления элемента "Item 3" с приоритетом 2 в очередь.
-            queue.Enqueue("Item 3", 2);
+        queue.Insert(index, item);
+        priorities.Insert(index, priority);
+    }
 
-            //цикл будет выполняться, пока очередь не пуста
-            while (!queue.IsEmpty)
+    //метод для удаления и возврата первого элемента очереди
+    public T Dequeue()
+    {
+        //Он выбрасывает исключение InvalidOperationException, если очередь пуста.
+        if (queue.Count == 0)
+            throw new InvalidOperationException("Queue is empty");
+
+        T item = queue[0];
+        queue.RemoveAt(0);
+        priorities.RemoveAt(0);
+        return item;
+    }
+
+    //свойство, возвращающее количество элементов в очереди.
+    public int Count
+    {
+        get { return queue.Count; }
+    }
+
+    //свойство, возвращающее значение true, если очередь пуста, и false в противном случае.
+    public bool IsEmpty
+    {
+        get { return queue.Count == 0; }
+    }
+
+    public void Clear()
+    {
+        queue.Clear();
+        priorities.Clear();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        PriorityQueue<string> queue = new PriorityQueue<string>();
+
+        Console.WriteLine("Введите элементы очереди (для завершения введите 'end'):");
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            Console.Write("Введите приоритет: ");
+            int priority;
+            while (!int.TryParse(Console.ReadLine(), out priority) || priority < 0)
             {
-                //вызов метода `Dequeue` для извлечения элемента с наивысшим приоритетом из очереди и присваивание его переменной `item`
-                string item = queue.Dequeue();
-                Console.WriteLine(item);
-            }    
+                    Console.WriteLine("Некорректный ввод, попробуйте еще раз:");
+            }
+            queue.Enqueue(input, priority);
+        }
+
+        Console.WriteLine("Очередь с приоритетами:");
+
+        while (!queue.IsEmpty)
+        {
+            string item = queue.Dequeue();
+            Console.WriteLine(item);
         }
     }
 }
--- part2.cs
+++ part2.cs
@@ -1,168 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace exampleLaba
+public class PriorityQueue<T>
 {
-    //Класс `Node` представляет узел в очереди и содержит два поля
-    public class Node
+    //объявляет приватное поле queue типа List<T> для хранения элементов очереди
+    private List<T> queue;
+    //oбъявляет приватное поле priorities типа List<int> для хранения приоритетов элементов очереди.
+    private List<int> priorities;
+
+    //Создает новые экземпляры List<T> и List<int> для queue и priorities соответственно.
+    public PriorityQueue()
     {
-        //`inf` типа `string`, которое хранит информацию об объекте
-        public string inf;
-        //`next` типа `Node`, которое указывает на следующий узел в очереди
-        public Node next;
+        queue = new List<T>();
+        priorities = new List<int>();
     }
-    //Класс `Program` содержит два статических поля: 
-    public class Program
+
+    //метод для добавления элемента item с приоритетом priority в очередь.
+    //Он находит правильную позицию в очереди, основываясь на приоритете, и вставляет элемент и приоритет в соответствующие списки.
+    public void Enqueue(T item, int priority)
     {
-        //Представляет начало очереди
-        public static Node head;
-        //Представляет конец очереди
-        public static Node last;
-
-        //
-        public static void Main(string[] args)
+        int index = queue.Count;
+        while (index > 0 && priority <= priorities[index - 1])
         {
-            string menuOption;
-            //В цикле выводится меню с опциями для выбора
-            //Пользователь вводит выбранную опцию, и в зависимости от
-            //этой опции вызывается соответствующий метод или выводится
-            //сообщение об ошибке в случае некорректного выбора
-            do
-            {
-                Console.WriteLine("Выберите опцию:");
-                Console.WriteLine("1.Добавить элемент в очередь");
-                Console.WriteLine("2.Посмотреть очередь");
-                Console.WriteLine("3.Удалить элемент из очереди");
-                Console.WriteLine("4.Выход");
-
-                menuOption = Console.ReadLine();
-
-                switch (menuOption)
-                {
-                    case "1":
-                        Enqueue();
-                        break;
-                    case "2":
-                        review();
-                        break;
-                    case "3":
-                        Dequeue();
-                        break;
-                    case "4":
-                        Console.WriteLine("Программа завершена");
-                        break;
-                    default:
-                        Console.WriteLine("Неверная опция, попробуйте ещё раз");
-                        break;
-                }
-                Console.WriteLine();
-            } while (menuOption != "4");
+            index--;
         }
 
-        //Метод добавляет новый узел в очередь.
-        public static void Enqueue()
+        queue.Insert(index, item);
+        priorities.Insert(index, priority);
+    }
+
+    //метод для удаления и возврата первого элемента очереди
+    public T Dequeue()
+    {
+        //Он выбрасывает исключение InvalidOperationException, если очередь пуста.
+        if (queue.Count == 0)
+            throw new InvalidOperationException("Queue is empty");
+
+        T item = queue[0];
+        queue.RemoveAt(0);
+        priorities.RemoveAt(0);
+        return item;
+    }
+
+    //свойство, возвращающее количество элементов в очереди.
+    public int Count
+    {
+        get { return queue.Count; }
+    }
+
+    //свойство, возвращающее значение true, если очередь пуста, и false в противном случае.
+    public bool IsEmpty
+    {
+        get { return queue.Count == 0; }
+    }
+
+    public void Clear()
+    {
+        queue.Clear();
+        priorities.Clear();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        PriorityQueue<string> queue = new PriorityQueue<string>();
+
+        Console.WriteLine("Введите элементы очереди (для завершения введите 'end'):");
+        string input;
+        while ((input = Console.ReadLine()) != "end")
         {
-            //Создается новый узел `p` с использованием метода `CreateNode()`
-            Node p = CreateNode();
-            //Если очередь пуста, то новый узел становится началом и концом очереди
-            if (head == null && p != null)
+            Console.Write("Введите приоритет: ");
+            int priority;
+            while (!int.TryParse(Console.ReadLine(), out priority) || priority < 0)
             {
-                head = p;
-                last = p;
+                    Console.WriteLine("Некорректный ввод, попробуйте еще раз:");
             }
-            //Если очередь не пуста, то новый узел добавляется в конец очереди, а `last` указывает на него.
-            else if (head != null && p != null)
-            { 
-                last.next = p;
-                last = p;
-            }
-            return;
+            queue.Enqueue(input, priority);
         }
 
-        //Метод `CreateNode` создает новый узел и запрашивает у пользователя данные об объекте.
-        public static Node CreateNode()
+        Console.WriteLine("Очередь с приоритетами:");
+
+        while (!queue.IsEmpty)
         {
-            Node p = new Node();
-            string s;
-
-            Console.WriteLine("Введите название объекта:");
-            s = Console.ReadLine();
-            //Если пользователь не ввел название объекта, то возвращается `null`
-            
-            if (string.IsNullOrEmpty(s))
-            {
-                Console.WriteLine("Запись не была произведена");
-                return null;
-            }
-            //Иначе, узел заполняется данными и возвращается.
-            p.inf = s;
-
-            p.next = null;
-
-            return p;
-        }
-
-        //Метод `review` выводит содержимое очереди
-        public static void review()
-        {
-            //Переменная `struc` указывает на первый узел в очереди
-            Node struc = head;
-            //Если очередь пуста, то выводится сообщение об этом
-            if (head == null)
-            {
-                Console.WriteLine("Очередь пуста");
-            }
-            //Иначе, в цикле выводится название объекта из каждого узла, пока не достигнется конец очереди.
-            while (struc != null)
-            {
-                Console.WriteLine("Имя - {0}", struc.inf);
-                struc = struc.next;
-            }
-            return;
-        }
-
-        //Метод `Dequeue` удаляет первый узел из очереди.
-        public static void Dequeue()
-        {
-            //Если очередь пуста, выводится сообщение об этом
-            if (head == null)
-            {
-                Console.WriteLine("Очередь пуста");
-                return;
-            }
-            //Иначе, первый узел удаляется из очереди, и информация об этом выводится на экран.
-            Node dequeuedNode = head;
-            head = head.next;
-            dequeuedNode.next = null;
-
-            Console.WriteLine("Элемент {0} удален из очереди", dequeuedNode.inf);
-        }
-
-        //Метод `Contains` проверяет, содержится ли объект с заданным именем в очереди
-        public static bool Contains(string name)
-        {
-            //Переменная `struc` указывает на первый узел в очереди
-            Node struc = head;
-            // Если очередь пуста, выводится сообщение об этом
-            if (head == null)
-            {
-                Console.WriteLine("Очередь пуста");
-            }
-            //Иначе, в цикле проверяется каждый узел на соответствие имени.
-            while (struc != null)
-            {
-                //Если совпадение найдено, возвращается `true`
-                if (name == struc.inf)
-                {
-                    return true;
-                }
-                struc = struc.next;
-            }
-            //Если цикл завершается без совпадений, возвращается `false`.
-            return false;
+            string item = queue.Dequeue();
+            Console.WriteLine(item);
         }
     }
 }
